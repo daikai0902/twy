8443e01345113e42a478a5b04ae8d5627637251d
redstoone <cy_jing_2010@163.com>
2018-03-18 12:37:32 +0800

    首页修改一波

--- twyClient/src/components/Home.vue
+++ twyClient/src/components/Home.vue
@@ -2,20 +2,63 @@
   <div class="index-wrap">
     <div class="index-room"></div>
     <div class="topbar">
-      <router-link :to="{name: 'order'}" class="txt-right pr45 order-link">预约报名</router-link>
+      <router-link :to="{name: 'order'}" class="order-link">预约报名</router-link>
+      <side-menu></side-menu>
     </div>
     <div class="index-intro">
       <div class="intro-title">天唯艺星教育</div>
       <div class="intro-desc">宁波天唯艺星教育是上海小荧星核心战略合作伙伴，由上海戏剧学院徐明德教授和27年资深教育管理者带领，携手各艺术领域专家教师，志在做最专业的艺术基础教育。</div>
       <div class="intro-link">学校简介  ></div>
     </div>
+
+    <div class="home-menu-wrap">
+      <div class="flex-demo flex-item1">
+        <img class="m-img" src="../assets/m1.png">
+        <p class="m-name">新闻·公告 ></p>
+      </div>
+       <div class="flex-demo flex-item1">
+        <img class="m-img" src="../assets/m1.png">
+        <p class="m-name">精彩活动 ></p>
+      </div>
+      <flexbox>
+        <flexbox-item>
+          <div class="flex-demo flex-item2">
+            <img class="m-img" src="../assets/m3.png">
+            <p class="m-name">课程介绍</p>
+            <p class="m-ename">Course introduction</p>
+          </div>
+        </flexbox-item>
+        <flexbox-item>
+          <div class="flex-demo flex-item2">
+            <img class="m-img" src="../assets/m4.png">
+            <p class="m-name">名师团队</p>
+            <p class="m-ename">Teacher Team</p>
+          </div>
+          </flexbox-item>
+        <flexbox-item>
+          <div class="flex-demo flex-item2">
+            <img class="m-img" src="../assets/m5.png">
+            <p class="m-name">公开课</p>
+            <p class="m-ename">Open class</p>
+          </div>
+        </flexbox-item>
+      </flexbox>
+    </div>
     <img class="bg-home" src="../assets/bg_home.jpg">
   </div>
 </template>
 
 <script>
+import { Flexbox, FlexboxItem } from 'vux'
+import SideMenu from '@/components/common/SideMenu'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Flexbox,
+    FlexboxItem,
+    SideMenu
+  }
 }
 </script>
 
@@ -27,5 +70,99 @@ export default {
   width: 100%;
   height: 100%;
   z-index: 1;
+}
+.topbar{
+  position: relative;
+  z-index: 10;
+  margin-top: 15px;
+  margin-right: 20px;
+  text-align: right;
+}
+.order-link{
+  width: 76px;
+  height: 30px;
+  line-height: 30px;
+  display: inline-block;
+  text-align: center;
+  color: #fff;
+  font-size:14px;
+  background:rgba(255,255,255,0.30);
+  border-radius:100px;
+}
+.index-intro{
+  z-index: 10;
+  position: relative;
+  color: #fff;
+  width: 320px;
+  margin: 0 auto;
+  padding-top: 75px;
+  box-sizing: border-box;
+}
+.index-intro .intro-title{
+  font-size:24px;
+  margin-bottom: 10px;
+}
+.index-intro .intro-desc,
+.index-intro .intro-link{
+  font-size: 12px;
+}
+.index-intro .intro-link{
+  margin-top: 10px;
+}
+.home-menu-wrap{
+  z-index: 10;
+  position: relative;
+  width: 320px;
+  margin: 0 auto;
+  margin-top: 88px;
+}
+.flex-item1{
+  background:rgba(255,255,255,0.40);
+  border-radius:12px;
+  width:198px;
+  height:50px;
+  font-size: 14px;
+  color:#000;
+  margin-bottom: 20px;
+  position: relative;
+  padding-left: 100px;
+  box-sizing: border-box;
+}
+.flex-item1 .m-img{
+  width:22px;
+  height:18px;
+  vertical-align: middle;
+  position: absolute;
+  left: 15px;
+  top: 16px;
+}
+.flex-item1 .m-name{
+  opacity:0.5;
+  text-align: right;
+  padding-right: 15px;
+  line-height: 50px;
+}
+.flex-item2{
+  background:rgba(255,255,255,0.40);
+  border-radius:12px;
+  width:102px;
+  height:102px;
+  font-size:13px;
+  color:#000;
+  padding-top: 8px;
+  padding-left: 6px;
+  box-sizing: border-box;
+}
+.flex-item2 .m-img{
+  height: 27px;
+  margin-bottom: 10px;
+  margin-top: 5px;
+}
+.flex-item2 .m-name{
+  opacity:0.5;
+}
+.flex-item2 .m-ename{
+  font-size: 10px;
+  opacity:0.5;
 }
 </style>
---
+++ twyClient/src/components/common/SideMenu.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="sidemenu-wrap">
+    <div class="btn-sidemenu" @click="sideMenu = true">
+      <img class="img-sidemenu" src="../../assets/btn_sidemenu.png" />
+    </div>
+    <div v-transfer-dom>
+      <popup v-model="sideMenu" position="left" width="100%">
+        <div class="side-menu">
+          <span class="vux-close" @click="sideMenu = false"></span>
+
+
+          <router-link :to="{name: home}" class="home-link">HOME</router-link>
+          <div classs="menu-wrap">
+            <router-link v-for="(item, index) in menuList" :key="index" :to="{name: item.pathname}" class="menu-link">{{item.name}}</router-link>
+          </div>
+        </div>
+      </popup>
+    </div>
+  </div>
+</template>
+
+<script>
+import { TransferDom, Popup } from 'vux'
+export default {
+  name: 'SideMenu',
+  directives: {
+    TransferDom
+  },
+  components: {
+    Popup
+  },
+  data () {
+    return {
+      sideMenu: false,
+      menuList: [
+        {
+          name: '新闻·公告',
+          pathname: 'choirSucc'
+        },
+        {
+          name: '精彩活动',
+          pathname: 'choirSucc'
+        },
+        {
+          name: '课程介绍',
+          pathname: 'news'
+        },
+        {
+          name: '名师团队',
+          pathname: 'news'
+        },
+        {
+          name: '孩子成长艺星之路',
+          pathname: 'news'
+        },
+        {
+          name: '网点布局',
+          pathname: 'news'
+        },
+        {
+          name: '如何报名',
+          pathname: 'news'
+        },
+        {
+          name: '预约报名',
+          pathname: 'news'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .sidemenu-wrap{
+    display: inline-block;
+    vertical-align: top;
+    margin-left: 10px;
+  }
+  .btn-sidemenu{
+    width: 30px;
+    height: 30px;
+    background:rgba(255,255,255,0.30);
+    border-radius:50%;
+    display: inline-block;
+    position: relative;
+  }
+  .img-sidemenu{
+    width:16px;
+    height:12px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+  }
+  .side-menu{
+    height: 100%;
+    background: url('~/src/assets/bg_menu.jpg') no-repeat center center;
+    background-size: cover;
+    position: relative;
+  }
+  .vux-close{
+    background: url('~/src/assets/close.png') no-repeat center center;
+    background-size: cover;
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    right: 20px;
+    top: 15px;
+  }
+  .home-link{
+    font-size:36px;
+    color:#ffffff;
+    display: block;
+    padding-left: 50px;
+    box-sizing: border-box;
+    margin-bottom: 20px;
+    padding-top: 130px;
+  }
+  .menu-link{
+    color:#fff;
+    display: block;
+    margin-top: 10px;
+    font-size: 16px;
+    padding-left: 50px;
+    box-sizing: border-box;
+  }
+</style>
+
+
