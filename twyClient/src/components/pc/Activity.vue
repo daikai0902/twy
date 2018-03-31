e893318ab23298c605c3348aa3b35744573cb8f2
kangbo_1991 <kangbo_1991@126.com>
2018-04-01 07:11:40 +0800

    'push_again'

--- twyClient/src/components/pc/Activity.vue
+++ twyClient/src/components/pc/Activity.vue
@@ -8,7 +8,7 @@
               <div class="box">
                 <div class="menu-list">
                 <img class="back" src="../../assets/pc/activity/back.png" alt="" @click="goback"> 
-                  <ul>
+                  <ul> 
                     <li v-for="(item, index) in activity" :class="{'actNews':actActivity === item.id}" @click="showNewsDetail(item.id)">
                       <span class="news-title">{{item.title}}</span><span class="news-time">{{item.time}}</span>
                     </li>
