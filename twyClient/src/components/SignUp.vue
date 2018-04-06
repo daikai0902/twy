3e01d7f07e2dc1d28376848a5e00159111cfea9c
redstoone <cy_jing_2010@163.com>
2018-04-06 22:45:49 +0800

    wepay

--- twyClient/src/components/SignUp.vue
+++ twyClient/src/components/SignUp.vue
@@ -279,7 +279,7 @@ export default {
                     timestamp: res2.data.timestatm, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                     nonceStr: res2.data.noceStr, // 支付签名随机串，不长于 32 位
                     package: 'prepay_id=' + res2.data.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-                    signType: res2.data.sign, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                    signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
                     paySign: res2.data.paySign, // 支付签名
                     success: function (res) {
                       that.$router.push({ name: 'wepaySucc', query: {n: that.name} })
