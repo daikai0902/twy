79c0ca279173fad4b79e31c82844f35c0e5cdb12
redstoone <cy_jing_2010@163.com>
2018-12-30 23:54:19 +0800

    添加扫码提示页面

---
+++ twyClient/src/components/Rebate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+    <div class="">{{ code }}</div>
+    <div class="">{{ userId }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      code: null,
+      userId: null
+    }
+  },
+  created () {
+    console.log(this.$route)
+    let _qString = this.$route.query
+    this.code = _qString.code
+    this.userId = _qString.state
+  },
+  methods: {
+    
+  },
+}
+</script>
+
