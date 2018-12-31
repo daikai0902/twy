193c0f769ef1f21ce018b38d5fc4a321ac255d56
redstoone <cy_jing_2010@163.com>
2018-12-31 11:41:34 +0800

    添加绑定状态

--- twyClient/src/components/Rebate.vue
+++ twyClient/src/components/Rebate.vue
@@ -1,25 +1,39 @@
 <template>
   <div class="">
+    <msg :title="msgTitle" :description="msgDesc" :icon="msgIcon"></msg>
     <div class="">{{ code }}</div>
     <div class="">{{ userId }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { Msg, Divider } from 'vux'
 export default {
+  components: { Msg, Divider },
   data () {
     return {
       code: null,
-      userId: null
+      userId: null,
+      msgTitle: '绑定成功',
+      msgDesc: '您绑定返利人成功',
+      msgIcon: 'success'
     }
   },
   created () {
-    console.log(this.$route)
     let _qString = this.$route.query
     this.code = _qString.code
     this.userId = _qString.state
+    this.bindingwxRebate()
   },
   methods: {
+    bindingwxRebate () {
+      axios.post(`https://ykapi.twyxedu.com/bms/rebate/user/openId/add?code=${this.code}&userId=${this.userId}`, '').then(res => {
+        console.log(res)
+      }).catch((error) => {
+        alert(error)
+      })
+    }
   }
 }
 </script>
